8a887fb3c608be806cded680f57a7f418aacb97e
Gleidson Freitas <36724182+GLEIDOSN@users.noreply.github.com>
2021-04-25 19:40:06 -0300

    First controller and Razor pages testd

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿        using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
 using System;
 using System.Collections.Generic;
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# course";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
